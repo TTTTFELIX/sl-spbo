000557d346b64d1e423026fb9585cda5fd733f06
TTTTFELIX <snie@cpp.edu>
2023-07-10 20:54:57 -0700

    jpa one to many

---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/example/curddemo/dao/AppDAOImpl.java
@@ -0,0 +1,136 @@
+package com.example.curddemo.dao;
+
+import com.example.curddemo.entity.Course;
+import com.example.curddemo.entity.Instructor;
+import com.example.curddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+//    define field the entity manager
+    private EntityManager entityManager;
+
+//    inject entity manager using constructor inject
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int id) {
+        return entityManager.find(Instructor.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int id) {
+
+//        retrieve the instructor
+        Instructor instructor = entityManager.find(Instructor.class, id);
+
+//        find course
+        List<Course> courses = instructor.getCourses();
+
+
+//        break associate of all the courses for the instructor
+        for (Course tempCourse: courses){
+            tempCourse.setInstructor(null);
+        }
+
+
+//        delete the instructor
+        entityManager.remove(instructor);
+
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+        return entityManager.find(InstructorDetail.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int id) {
+
+        //        retrieve the instructorDetail
+        InstructorDetail instructorDetail = entityManager.find(InstructorDetail.class, id);
+
+//        remove the associated object reference
+//        break bi-directional link
+        instructorDetail.getInstructor().setInstructorDetail(null);
+
+//        delete the instructorDetail
+        entityManager.remove(instructorDetail);
+
+    }
+
+    @Override
+    public List<Course> findCoursesByInstructorId(int Id) {
+
+//        create query
+        TypedQuery<Course> query = entityManager.createQuery(
+                "from Course where instructor.id = :data", Course.class);
+
+        query.setParameter("data", Id);
+
+        List<Course> courses = query.getResultList();
+
+        return courses;
+
+    }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int Id) {
+
+        //        create query
+        TypedQuery<Instructor> query = entityManager.createQuery(
+                "select i from Instructor i Join FETCH i.courses where i.id = :data", Instructor.class);
+
+        query.setParameter("data", Id);
+
+        Instructor instructor = query.getSingleResult();
+
+        return instructor;
+    }
+
+    @Override
+    @Transactional
+    public void update(Instructor tempInstructor) {
+        entityManager.merge(tempInstructor);
+    }
+
+    @Override
+    @Transactional
+    public void update(Course tempCourse) {
+        entityManager.merge(tempCourse);
+
+    }
+
+    @Override
+    public Course findCourseById(int id) {
+        return entityManager.find(Course.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCourseById(int id) {
+
+        Course course = entityManager.find(Course.class, id);
+
+        entityManager.remove(course);
+
+    }
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/example/curddemo/dao/AppDAO.java
@@ -0,0 +1,37 @@
+package com.example.curddemo.dao;
+
+import com.example.curddemo.entity.Course;
+import com.example.curddemo.entity.Instructor;
+import com.example.curddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor instructor);
+
+     Instructor findInstructorById(int id);
+
+     void deleteInstructorById(int id);
+
+     InstructorDetail findInstructorDetailById(int theId);
+
+     void deleteInstructorDetailById(int id);
+
+     List<Course> findCoursesByInstructorId(int Id);
+
+     Instructor findInstructorByIdJoinFetch(int Id);
+
+     void update(Instructor tempInstructor);
+
+     void update(Course tempCourse);
+
+     Course findCourseById(int id);
+
+     void deleteCourseById(int id);
+
+     void save(Course theCourse);
+
+     Course findCourseAndReviewByCourseId(int theId);
+
+}
